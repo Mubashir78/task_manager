8b62648c76fe1b9030857cd213f14fdad4859f9b
Navashub <navasmuller01@gmail.com>
2026-06-18 23:12:15 +0300

    Add the CRUD file

---
+++ crud.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+import models, schemas 
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+
+
+def create_task(db:Session, task: schemas.TaskCreate):
+    db_task = models.Task(**task.model_dump())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def get_task(db:Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
+
+def update_task(db:Session, task_id: int, data: schemas.TaskUpdate):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not db_task:
+        return None 
+    
+    updates = data.model_dump(exclude_unset=True)
+    for field, value in updates.items():
+        setattr(db_task, field, value)
+
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+
+def delete_task(db: Session, task_id: int):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not db_task:
+        return None
+    db.delete(db_task)
+    db.commit()
+    return db_task
